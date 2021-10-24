f2ea644808bc9dcde12eb0e3d30d017ad1abf9d4
TottySnowman <65237023+TottySnowman@users.noreply.github.com>
2021-10-24 22:57:17 +0200

    fixed no face found and destroyImage

---
+++ assets/js/basic_functions.js
@@ -0,0 +1,81 @@
+let base64_image_string = '';
+const image_to_upload = document.getElementById("image_upload");
+const preview_image = document.getElementById("image_preview");
+
+image_to_upload.addEventListener("change", function(){
+    const selected_image = this.files[0];
+
+    if(selected_image)
+    {
+        const base_reader = new FileReader();
+        base_reader.addEventListener("load", function(){
+            base64_image_string = this.result;
+            preview_image.setAttribute("src", base64_image_string);
+        })
+
+        base_reader.readAsDataURL(selected_image);
+    }
+})
+
+document.getElementById("snapshot_button").onclick = async () =>
+{
+    //Webcam element wird erstellt
+    let webcam_display = document.createElement("video");
+
+    //set attributes to webcam
+    webcam_display.id = 'webcam_display';
+    webcam_display.setAttribute("autoplay", "");
+    webcam_display.setAttribute("playinline", "");
+    webcam_display.style.opacity = "0";
+
+    //display webcam to site and do a screenshot
+    document.body.appendChild(webcam_display);
+    const canvasElement = document.getElementById("webcam_canvas");
+    const webcam = new Webcam(webcam_display, "user", canvasElement);
+    
+    //starts webcam and waits 2 seconds to be really sure that the webcam is active
+    webcam.start();
+    await new Promise(start_up => setTimeout(start_up, 2000));
+    
+    //creates a snapshot and displays to the site
+    base64_image_string = webcam.snap();
+    preview_image.setAttribute("src", base64_image_string);
+
+    //removes webcam from site and stops it
+    webcam.stop();
+    webcam_display.remove();
+}
+
+document.getElementById("btn_upload_image").onclick = function()
+{
+    var stringLength = base64_image_string.length;
+    var sizeInBytes = 4 * Math.ceil((stringLength / 3))*0.5624896334383812;
+    var sizeInKb=sizeInBytes/1000;
+
+    if(sizeInKb > 2000)
+    {
+        $('#infoMessages').html('<div class="alert alert-danger" role="alert">The file is too big! Please select another file!</div>');
+    }
+    else
+    {
+        jQuery.ajax({
+            type: "POST",
+            url: "../Emognition/upload_beautify.php",
+            dataType: "json",
+            data: {functionname: "upload_image_to_server", arguments: [base64_image_string]},
+            success: function(obj, textstatus)
+            {
+                if(!('error' in obj))
+                {
+                    //wenn the array error is empty starting to detect emotions of the image
+                    var image_path = obj.result;
+                    load_face_api(image_path);
+                }
+                else
+                {
+                    alert(obj.error);
+                }
+            }
+        })
+    }
+}
---
+++ edit_image_beautify.php
@@ -0,0 +1,74 @@
+<?php
+    //header for response
+    header('Content-Type: application/json');
+
+    //is used to write the results and gets read in js
+    $aResult = array();
+
+    //function to edit the picture(puts emoji on the face)
+    function edit_image($box_pos_x, $box_pos_y, $box_width, $box_height, $emotion, $pictureSource)
+    {
+        //create gd library object with the emotion
+        $base_emotion_image = imagecreatefrompng("assets/img/Emotions/" . $emotion . ".png");
+        
+        //gets width and height from base image
+        $base_emotion_width  = imagesx($base_emotion_image);
+        $base_emotion_height = imagesy($base_emotion_image);
+
+        //cuts emoji pic to fit it on the head
+        $cutted_emotion_image = imagecreate($box_width, $box_height);
+        imagecopyresampled($cutted_emotion_image, $base_emotion_image, 0, 0, 0, 0, $box_width, $box_height, $base_emotion_width, $base_emotion_height);
+
+        //creates gd library object from picturesource
+        $base_image = imagecreatefrompng($pictureSource);
+
+        imagecopy($base_image, $cutted_emotion_image, $box_pos_x , $box_pos_y, 0, 0, $box_width, $box_height);
+
+        $current_date = date("d-m-y");
+        $current_time = date("H:i:s");
+        $manipulated_pic_path = 'assets/img/output/' . uniqid() . '_' . $current_date . '_' . $current_time . '_manipulated.png';
+        
+        //creates image on server
+        imagepng($base_image, $manipulated_pic_path);
+
+        //destroy gd library objects
+        imagedestroy($base_emotion_image);
+        imagedestroy($cutted_emotion_image);
+        imagedestroy($base_image);
+
+        return $manipulated_pic_path;
+    }
+
+    if( !isset($_POST['functionname']) ) 
+    { 
+        $aResult['error'] = 'No function name!'; 
+    }
+
+    if( !isset($_POST['arguments']) )
+    { 
+        $aResult['error'] = 'No function arguments!'; 
+    }
+
+    if( !isset($aResult['error']) ) 
+    {
+        switch($_POST['functionname']) 
+        {
+            case 'edit_image':
+                if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 3) ) 
+                {
+                    $aResult['error'] = 'Error in arguments!';
+                }
+                else 
+                {
+                    $aResult['result']  = edit_image($_POST['arguments'][0], $_POST['arguments'][1], $_POST['arguments'][2], $_POST['arguments'][3], $_POST['arguments'][4], $_POST['arguments'][5]);
+                }
+            break;
+
+            default:
+                $aResult['error'] = 'Not found function '.$_POST['functionname'].'!';
+            break;
+    }
+
+    }
+    echo json_encode($aResult);
+?>
